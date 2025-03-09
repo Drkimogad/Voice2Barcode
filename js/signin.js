c633757e91ef50bbee3b2e472034b092dda4ee66
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-09 11:36:21 +0200

    Update signin.js

--- js/signin.js
+++ js/signin.js
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Store authentication status
             localStorage.setItem('authToken', 'dummy-token');
+            console.log('Saving Token:', 'dummy-token');
             console.log('Auth Token:', localStorage.getItem('authToken'));
 
             // Redirect to dashboard
